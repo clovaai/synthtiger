f062d774ba4a96a743802f1f848eea92239b50f6
moonbings <moonbings@gmail.com>
2022-11-09 01:29:18 +0900

    Add seed option

--- synthtiger/gen.py
+++ synthtiger/gen.py
@@ -4,12 +4,14 @@ Copyright (c) 2021-present NAVER Corp.
 MIT license
 """
 
+import itertools
 import os
 import random
 import sys
 import traceback
 from multiprocessing import Process, Queue
 
+import imgaug
 import numpy as np
 import yaml
 
@@ -31,21 +33,33 @@ def read_config(path):
     return config
 
 
-def generator(path, name, config=None, worker=0, verbose=False):
-    if worker > 0:
-        queue = Queue(maxsize=1024)
-        for _ in range(worker):
-            _run(_worker, (path, name, config, queue, verbose))
+def generator(path, name, config=None, count=None, worker=0, seed=None, verbose=False):
+    counter = range(count) if count is not None else itertools.count()
+    tasks = _task_generator(seed)
 
-        while True:
-            data = queue.get()
-            yield data
+    if worker > 0:
+        task_queue = Queue(maxsize=worker)
+        data_queue = Queue(maxsize=worker)
+        pre_count = min(worker, count) if count is not None else worker
+        post_count = count - pre_count if count is not None else None
+
+        for _ in range(worker):
+            _run(_worker, (path, name, config, task_queue, data_queue, verbose))
+        for _ in range(pre_count):
+            task_queue.put(next(tasks))
+
+        for idx in counter:
+            task_idx, data = data_queue.get()
+            if post_count is None or idx < post_count:
+                task_queue.put(next(tasks))
+            yield task_idx, data
     else:
         template = read_template(path, name, config)
 
-        while True:
-            data = _generate(template, verbose)
-            yield data
+        for _ in counter:
+            task_idx, task_seed = next(tasks)
+            data = _generate(template, task_seed, verbose)
+            yield task_idx, data
 
 
 def _run(func, args):
@@ -55,17 +69,34 @@ def _run(func, args):
     return proc
 
 
-def _worker(path, name, config, queue, verbose):
-    random.seed()
-    np.random.seed()
+def _task_generator(seed):
+    random_generator = random.Random(seed)
+    task_idx = -1
+
+    while True:
+        task_idx += 1
+        task_seed = random_generator.getrandbits(128)
+        yield task_idx, task_seed
+
+
+def _worker(path, name, config, task_queue, data_queue, verbose):
     template = read_template(path, name, config)
 
     while True:
-        data = _generate(template, verbose)
-        queue.put(data)
+        task_idx, task_seed = task_queue.get()
+        data = _generate(template, task_seed, verbose)
+        data_queue.put((task_idx, data))
 
 
-def _generate(template, verbose):
+def _generate(template, seed, verbose):
+    temp_state = random.getstate()
+    temp_np_state = np.random.get_state()
+    temp_imgaug_state = imgaug.random.get_global_rng().state
+
+    random.seed(seed)
+    np.random.set_state(np.random.RandomState(np.random.MT19937(seed)).get_state())
+    imgaug.seed(seed)
+
     while True:
         try:
             data = template.generate()
@@ -73,4 +104,10 @@ def _generate(template, verbose):
             if verbose:
                 print(f"{traceback.format_exc()}")
             continue
-        return data
+        break
+
+    random.setstate(temp_state)
+    np.random.set_state(temp_np_state)
+    imgaug.random.get_global_rng().state = temp_imgaug_state
+
+    return data
